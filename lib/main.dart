da6821e299f19f735f35d69ad17472e12b6fe598
sudarlife <sudarlife@sgma.io>
2024-07-15 13:20:40 +0900

    파이어베이스 프로젝트 연동

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:bamtol_market_app/firebase_options.dart';
 import 'package:bamtol_market_app/src/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
